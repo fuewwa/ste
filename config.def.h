568f6e0519a66be4f139a458209f52160e5d8c0d
KURWAss <kurwa.offc@proton.me>
2026-09-24 09:24:05 +0300

    Added config.def.h

---
+++ config.def.h
@@ -0,0 +1,9 @@
+#pragma once
+
+static const char quit = 'q';
+static const char writeMode = 'w';
+static const char save = 's';
+static const int escape = 27;
+
+static const int tab = 4;
+static const bool lines = false;
