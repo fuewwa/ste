0471a9879f7f5a3035389d949597d43f96d97cc8
KURWAss <kurwa.offc@proton.me>
2026-09-24 09:21:13 +0300

    Deleted config.h

--- config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-static const char quit = 'q';
-static const char writeMode = 'w';
-static const char save = 's';
-static const int escape = 27;
-
-static const int tab = 4;
-static const bool lines = false;
